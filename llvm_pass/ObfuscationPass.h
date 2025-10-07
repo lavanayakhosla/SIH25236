9d58959739c1b5aeba9f3362eb004d3f634818b5
lavanayakhosla <144594627+lavanayakhosla@users.noreply.github.com>
2025-10-07 10:45:51 +0530

    Create ObfuscationPass.h

---
+++ llvm_pass/ObfuscationPass.h
@@ -0,0 +1,21 @@
+#ifndef OBFUSCATION_PASS_H
+#define OBFUSCATION_PASS_H
+
+#include "llvm/IR/PassManager.h"
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Constants.h"
+
+using namespace llvm;
+
+namespace obf {
+struct ObfuscationOptions {
+  unsigned bogusBlocksPerFunction = 1;
+  unsigned stringEncryptLevel = 1;
+  unsigned insertNops = 0;
+};
+}
+
+#endif
